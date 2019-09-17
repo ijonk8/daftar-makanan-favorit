ceba8772fc0fbe947ba849e8bc896d8e28ca17e5
ijonk7 <41526209+ijonk7@users.noreply.github.com>
2019-09-17 23:40:31 +0700

    Menghapus Gado-Gado & menambahkan Donat
    Membuat file makan-pagi.php Part 3

--- makan-pagi.php
+++ makan-pagi.php
@@ -4,7 +4,6 @@ echo 'Makan pagi adalah nasi goreng' . "<br>";
 echo 'Bubur ayam' . "<br>";
 echo 'Atau Mie ayam';
 
-
-echo 'Makan Gado-Gado' . "<br>";
-
 echo 'Dan lontong sayur';
+
+echo 'Nyemil Donat';
