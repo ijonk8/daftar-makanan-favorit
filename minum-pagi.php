71e5c23cbb39a4153f10fe7ce89b87a352ceb15f
ijonk7 <41526209+ijonk7@users.noreply.github.com>
2019-09-18 16:22:51 +0700

    Update file minum-pagi.php - Part 4
    Menambahkan Minuman Milo Hangat

--- minum-pagi.php
+++ minum-pagi.php
@@ -4,6 +4,7 @@ echo 'Minuman pagi adalah Air Putih' . "<br>";
 echo 'Teh Hangat';
 
 echo 'Minuman pagi adalah Jeruk Hangat' . "<br>";
+echo 'Milo Hangat';
 
 echo 'Minuman pagi adalah Air Susu' . "<br>";
 echo 'Jahe Hangat';
